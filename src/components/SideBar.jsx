29be11b09fea5ef2cfd55918b40c4c1be30aba2b
iuryroque <iury.roque@hotmail.com>
2021-10-11 18:52:18 -0300

    Criacao e estilizacao da side bar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,36 @@
+
+import React from "react";
+import './SideBar.css'
+
+function SideBar() {
+  return(
+    <div className='side-bar'>
+      <label title='LINGUAGEM' for='linguagem'>
+        <button id='linguagem' className='btn-side-bar linguagem ' >LINGUAGEM</button>
+      </label>
+      <label title='RESISTÊNCIA' for='resistencia' className='label-side-bar'>
+        <button id='resistencia' className='btn-side-bar resistencia'>RESISTÊNCIA</button>
+      </label>
+      <label title='SERIEDADE' for='seriedade' className='label-side-bar'>
+        <button id='SERIEDADE' className='btn-side-bar seriedade'>SERIEDADE</button>
+      </label>
+      <label title='AUTONOMIA' for='autonomia' className='label-side-bar'>
+        <button id='autonomia' className='btn-side-bar autonomia'>AUTONOMIA</button>
+      </label>
+      <label title='MOVIMENTO' for='movimento' className='label-side-bar'>
+        <button id='movimento' className='btn-side-bar movimento'>MOVIMENTO</button>
+      </label>
+      <label title='FELICIDADE' for='felicidade' className='label-side-bar'>
+        <button id='felicidade' className='btn-side-bar felicidade'>FELICIDADE</button>
+      </label>
+      <label title='TRANSFORMAÇÃO' for='transformacao' className='label-side-bar'>
+        <button id='transformacao' className='btn-side-bar transformacao'>TRANSFORMAÇÃO</button>
+      </label>
+      <label title='SAUDADE' for='saudade' className='label-side-bar'>
+        <button id='saudade' className='btn-side-bar saudade'>SAUDADE</button>
+      </label>
+    </div>
+  )
+}
+
+export default SideBar;
