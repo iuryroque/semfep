6092c4df60815785786fe8835f11eade12cdefd5
uroque <ulissesroque@protonmail.com>
2021-10-12 14:22:11 -0300

    Adiciona a home do evento com responsividade - concluido

--- src/pages/semfepHome.jsx
+++ src/pages/semfepHome.jsx
@@ -1,21 +1,19 @@
 import '../App.css';
 import './semfepHome.css';
 import { Link } from 'react-router-dom';
-import Frame2 from '../images/backgrounds/Frame2.png'
-import ButtonPlay from '../images/Button-Play.png'
-
+import Frame2 from '../images/backgrounds/Frame2.png';
+import ButtonPlay from '../images/Button-Play.png';
 
 function SemfepHomePage() {
   return (
     <div className="page-semfep">
-      <img className='background-semfep' src={ Frame2 } alt="" />
       <div className="comecar-container-semfep">
-        <h2 className='h2-semfep'>
+        <h2 className="h2-semfep">
           CLIQUE AQUI <br />
           PARA COMEÇAR
         </h2>
-        <Link className="button-play-semfep"to='/semfep/frames'>
-          <img className='img-button' src={ ButtonPlay } alt="" />
+        <Link className="button-play-semfep" to="/semfep/frames">
+          <img className="img-button" src={ButtonPlay} alt="" />
         </Link>
       </div>
     </div>
