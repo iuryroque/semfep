64965ba9a266133c2756fadb6a774a9ca4160a49
iuryroque <iury.roque@hotmail.com>
2021-10-12 12:50:57 -0300

    Ajuste tela Semfep Desktop

--- src/pages/semfepHome.jsx
+++ src/pages/semfepHome.jsx
@@ -1,18 +1,21 @@
 import '../App.css';
 import './semfepHome.css';
 import { Link } from 'react-router-dom';
+import Frame2 from '../images/backgrounds/Frame2.png'
+import ButtonPlay from '../images/Button-Play.png'
 
 
 function SemfepHomePage() {
   return (
     <div className="page-semfep">
+      <img className='background-semfep' src={ Frame2 } alt="" />
       <div className="comecar-container-semfep">
         <h2 className='h2-semfep'>
           CLIQUE AQUI <br />
           PARA COMEÇAR
         </h2>
-        <Link to='/semfep/frames'>
-          <button className="button-play-semfep"></button>
+        <Link className="button-play-semfep"to='/semfep/frames'>
+          <img className='img-button' src={ ButtonPlay } alt="" />
         </Link>
       </div>
     </div>
