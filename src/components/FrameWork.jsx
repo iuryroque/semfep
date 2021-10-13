6230e71476436f522a852ed512923c3376b16d2f
uroque <ulissesroque@protonmail.com>
2021-10-12 21:25:22 -0300

    Adiciona modificaçoes nos arquivos de framework e frames description

---
+++ src/components/FrameWork.jsx
@@ -0,0 +1,204 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './FrameWork.css';
+
+class FrameWork extends React.Component {
+  render() {
+    return (
+      <div>
+        <section className="container-framework">
+          <h1 className="title-framework">
+            Informações Sobre Enquadramento e Submissão de Trabalhos
+          </h1>
+          <h3 className="subtitle-framework">UM FRAME PARA CHAMAR DE MEU</h3>
+          <p>
+            O Grupo FEP está debutando!!! E neste ano da graça de 2021 em que
+            comemoramos nossas 15 primaveras, quisemos ser provocantes, que tal
+            um evento sem tema? É com esse ímpeto que VI SEMFEP pretende
+            problematizar o modo peculiar com que são propagadas pelas redes
+            sociais algumas das visões mais comuns sobre a educação brasileira,
+            que produzem ou remetem a territórios bem consolidados, como se pode
+            observar pelo uso ostensivo que fazem de palavras de ordem, slogans
+            e hashtags.
+          </p>
+          <p>
+            Quem esteve em navegando pelo Instagram ou Facebook nos últimos
+            meses, ou por outros lugares do ambiente digital veiculadores dos
+            discursos da área da Educação, com olhos atentos, vislumbra certos
+            enquadramentos comuns e persistentes, as pequenas peças de
+            divulgação propagadas por ocasião da campanha para Ato Público
+            Nacional Educação Contra a Barbárie, deflagrado pela Universidade
+            Federal da Bahia (maio/2021), é um dos tantos exemplos a que podemos
+            nos remeter.{' '}
+          </p>
+          <p>
+            Neste VI SEMFEP, tomaremos proveito dessas derivas que temos
+            realizado através dos portais, plataformas e redes sociais, e suas
+            intersecções com os nossos cotidianos em exercício de formação de
+            professores, para problematizar o modo peculiar que fazem uso as
+            diversas publicações enquadradas no plano educacional a que temos
+            sido expostos diuturnamente ao atravessarem as circunstâncias
+            geohistóricas em que cada um de nós estamos imersos, e que
+            conceituações e teorizações podemos produzir a partir das palavras
+            de ordem e slogans disseminados como pertencentes à educação
+            brasileira, aqui, para as finalidades do evento e da submissão de
+            trabalhos, identificadas como <strong>Frames</strong>.
+          </p>
+          <p>
+            Palavra estrangeira cunhada no âmbito da técnica de fotografia e
+            cinema, o frame (lê-se: frei-me), quer dizer em tradução direta
+            “quadro”, é daí que podemos derivar para enquadramento, mas podemos
+            percebê-lo também como um emolduramento, no sentido de que o Frame é
+            uma demarcação do que se vê. Em um texto para a Revista Cult de maio
+            deste ano, que foi a inspiração para a produção VI SEMFEP, o
+            Professor Wilson Gomes define Frame tal como o que “resulta do
+            emprego de um ângulo de abordagem e da adoção de um ponto de vista,
+            (...) uma perspectiva. Por definição, um Frame, um enquadramento,
+            não nos dá toda a paisagem, impondo os limites do que conseguimos
+            ver, mas na nossa experiência nos parece sempre aquela janela pela
+            qual olhamos o mundo [como] está [e] tudo o que poderia ser visto”.
+          </p>
+          <p>
+            Sobre o plano da educação brasileira, o VI SEMFEP delimitou os
+            seguintes e provisórios Frames, com suas respectivas
+            problematizações fundamentadas em referências artísticas:
+            RESISTÊNCIA: Resistir é preciso? SERIEDADE: Para que tanta
+            seriedade, quer virar herói ou heroína? MOVIMENTO: E se movimentar
+            for sempre ter uma pedra no caminho? FELICIDADE: Tristeza não tem
+            fim, felicidade sim? AUTONOMIA: Autonomizar na educação para
+            (continuar a) morrer pela pátria e viver sem razão? LINGUAGEM: Viver
+            sem as viradas de páginas ou morrer de lamber os dedos?
+            TRANSFORMAÇÃO: E se pra mudar, bastar existir? SAUDADES: Chega de
+            Saudades?
+          </p>
+          <p>
+            Sobre o plano da educação brasileira, o VI SEMFEP delimitou os
+            seguintes e provisórios Frames, com suas respectivas
+            problematizações fundamentadas em referências artísticas:
+          </p>
+          <p>
+            {' '}
+            <strong>RESISTÊNCIA:</strong> Resistir é preciso?
+          </p>
+          <p>
+            <strong>SERIEDADE:</strong> Para que tanta seriedade, quer virar
+            herói ou heroína?
+          </p>
+          <p>
+            <strong>MOVIMENTO:</strong> E se movimentar for sempre ter uma pedra
+            no caminho?
+          </p>
+          <p>
+            <strong>FELICIDADE:</strong> Tristeza não tem fim, felicidade sim?
+          </p>
+          <p>
+            <strong>AUTONOMIA:</strong> Autonomizar na educação para (continuar
+            a) morrer pela pátria e viver sem razão?
+          </p>
+          <p>
+            <strong>LINGUAGEM:</strong> Viver sem as viradas de páginas ou
+            morrer de lamber os dedos?
+          </p>
+          <p>
+            <strong>TRANSFORMAÇÃO:</strong> E se pra mudar, bastar existir?
+          </p>
+          <p>
+            <strong>SAUDADES:</strong> Chega de Saudades?
+          </p>
+          <p>
+            Se você está aqui nesta área dedicada ao enquadrar seu trabalho, é
+            porque já deve ter escolhido um Frame para chamar de seu. Se isso
+            ainda não aconteceu, recomendamos que volte para a{' '}
+            <Link to="/semfep/frames"> página inicial do VI SEMFEP </Link>, e
+            busque por qual perspectiva mais te provoca sensações antes de
+            submeter seu trabalho. Mas se ao contrário, você já derivou por
+            todos os frames, e os limites para sua paisagem teorizante ou
+            conceitual já foram definidos, seguem algumas as regras do nosso
+            jogo.
+          </p>
+          <p>
+            <strong> Regras de Enquadramento e Submissão de Trabalhos </strong>
+          </p>
+          <ol>
+            <li>
+              Você deverá se inscrever no VI SEMFEP através da página do Sistema
+              de Gerenciamento de Eventos da UFBA - GERE (texto com link)
+              (http://www.gere.ufba.br/);
+            </li>
+            <li>
+              Realizada a inscrição, você deverá submeter para apreciação, um
+              texto em formato de áudio ou vídeo de no mínimo XX minutos, e
+              máximo XX minutos, com suas teorizações, conceituações, ou mesmo
+              expressão poética, sobre o Frame que escolheu;
+            </li>
+            <li>
+              O arquivo de áudio ou vídeo precisa ser anexado no ato da
+              submissão, e infelizmente não poderemos nos responsabilizar por
+              problemas técnicos seja no ato do envio, ou em casos de arquivos
+              corrompidos;
+            </li>
+            <li>
+              Os arquivos de áudio SOMENTE podem ser enviados em formato MP3,
+              Wave ou M4A. Existem muitos aplicativos gratuitos tanto para
+              celulares (smartphones), quanto para notebooks e computadores, que
+              gravam áudio na maioria nesses formatos;
+            </li>
+            <li>
+              Os arquivos de vídeos SOMENTE podem ser enviados em formato MP4,
+              MPEG, MKV e WMV. A maioria dos celulares (smartphones), gravam
+              vídeos em formato MP4;
+            </li>
+            <li>
+              No formulário de submissão do trabalho no GERE, também será pedido
+              que além do arquivo, você possa escrever um pequeno texto de no
+              mínimo XXX caracteres e no máximo XXX caracteres, para nos contar
+              um pouco sobre as intenções contidas no áudio ou o vídeo que você
+              enviou;
+            </li>
+            <li>
+              Ambos os textos, sejam os que foram submetidos em áudio e vídeo,
+              quanto os escritos no formulário,{' '}
+              <strong>devem ser totalmente originais</strong>, portanto,
+              pertencer a suas respectivas autoralidades e ainda não terem sido
+              comunicados em outros eventos ou publicações. Cada autoralidade,
+              de forma individual ou coletiva, será apreciada com base nas
+              referências que implicam o enquadramento de seus trabalhos;
+            </li>
+            <li>
+              Desde que a perspectiva seja a de localizar seus trabalhos nos
+              Frames que foram delimitados por ocasião do VI SEMFEP, em qualquer
+              um desses enquadramentos você poderá submeter trabalhos para
+              apreciação em qualquer área, categorias ou paradigmas de pesquisa
+              em educação e adjacências com a qual tenha afinidade;
+            </li>
+            <li>
+              O que a equipe de apreciação de trabalhos espera, é que do lugar
+              onde sua pesquisa atualmente se situa, você possa no áudio ou
+              vídeo e texto, se expressar de modo a problematizar o Frame que
+              escolheu, sem configuar uma tomada de posição adesista e
+              laudatória;{' '}
+            </li>
+            <li>
+              Serão aceitos trabalhos na seguinte condição de autoralidade: até
+              01 (Um) trabalho como autor principal; e até 01 (Um) trabalho como
+              autor secundário;
+            </li>
+            <li>
+              Ao submeter o trabalho, você concorda com a divulgação dos seus
+              áudios, vídeos e textos através de quaisquer plataformas digitais
+              escolhidas oportunamente pela produção do VI SEMFEP.
+            </li>
+          </ol>
+          <p>
+            BAIXE AS REGRAS DE ENQUADRAMENTO E SUBMISSÃO DE TRABALHOS, QUE CONTA
+            COM UMA TABELA COMPARATIVA DOS FRAMES EM FORMATO PDF CLICANDO{' '}
+            <strong>AQUI</strong>.
+          </p>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default FrameWork;
---
+++ src/components/FramesDescription.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FramesDescription.css';
+
+class FramesDescription extends React.Component {
+  render() {
+    return (
+      <div className="container-frames-description">
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default FramesDescription;
