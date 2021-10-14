bced1fc9c9cce6371ca27c76e14e3a97bef576c5
uroque <ulissesroque@protonmail.com>
2021-10-13 22:42:14 -0300

    Adiciona arquivos de componentes para paginas de framesDescription

---
+++ src/pages/framesDescription/resistenciaPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import SideBar from '../components/SideBar';
+import FramesDescription from '../components/FramesDescription';
+
+import '../App.css';
+import './felicidadePage.css';
+
+function FramesDescriptionPage() {
+  return (
+    <div className="page-frames-description">
+      <NavBar />
+      <SideBar />
+      <FramesDescription />
+      <Footer />
+    </div>
+  );
+}
+
+export default FramesDescriptionPage;
